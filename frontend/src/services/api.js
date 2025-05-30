ed6091646dc39d746781aa6616e1addfcf0b968c
patelchintan18018 <50191312+patelchintan18018@users.noreply.github.com>
2025-05-30 20:18:53 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/feedback',
+ /* baseURL: 'http://localhost:5000/api/feedback',*/
+   baseURL: 'https://catallyst-assignment1.vercel.app/api/feedback',
 });
 
 export default api;
